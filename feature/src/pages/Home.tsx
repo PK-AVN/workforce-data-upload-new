201355d08ea93da815cd869d6fda3813aabdd01d
ershubgupta <ershubgupta@gmail.com>
2023-09-26 13:14:31 +0530

    removed unused imports

--- feature/src/pages/Home.tsx
+++ feature/src/pages/Home.tsx
@@ -1,9 +1,5 @@
 import React, { useEffect } from "react";
-import FileUpload from "../components/forms/FileUpload/FileUpload";
-import { FileUploadType, CompleteColumnLabel } from "../common/constant";
-import GeneratorTemplate from "../components/GenerateTemplate/GenerateTemplate";
 import * as API from "../apiConfig/api";
-import IndividualGrid from "../IndividualGrid";
 
 const Home = () => {
   useEffect(() => {
